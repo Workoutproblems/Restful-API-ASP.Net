a6cd57bada001a59e6eb3875e779b54cd14f56fb
Johnny Soto <Johnny_soto@outlook.com>
2020-11-15 05:59:21 -0600

    small change on starup.

--- Parky/ParkyAPI/Startup.cs
+++ Parky/ParkyAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace ParkyAPI
         {
             Configuration = configuration;
         }
-
+        //small change.
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
