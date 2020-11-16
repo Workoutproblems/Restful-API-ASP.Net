c89e03c229d97ea595d017ea0cfb97b5ae505852
Johnny Soto <Johnny_soto@outlook.com>
2020-11-16 04:49:08 -0600

    Parky web project setup complete.

--- Parky/ParkyAPI/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Parky/ParkyAPI/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -36,6 +36,9 @@ namespace ParkyAPI.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
+                    b.Property<byte[]>("Picture")
+                        .HasColumnType("varbinary(max)");
+
                     b.Property<string>("State")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
