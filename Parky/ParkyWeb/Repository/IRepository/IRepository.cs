8f29feaccbf89fd11ba70d02437bfd0dcc3baa80
Johnny Soto <Johnny_soto@outlook.com>
2020-11-17 23:21:12 -0600

    Parkey web repository

---
+++ Parky/ParkyWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string url, int id);
+        Task<IEnumerable<T>> GetAllAsync(string url);
+        Task<bool> CreateAsync(String url, T objCreate);
+        Task<bool> UpdateAsync(String url, T objUpdate);
+        Task<bool> DeleteAsync(String url, int Id);
+
+    }
+}
