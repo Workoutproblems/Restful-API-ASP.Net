3c3ff9060832da06a6c917b1a2ef0093af015dc8
Johnny Soto <Johnny_soto@outlook.com>
2020-12-10 03:08:54 -0600

    Trails API completed.

--- Parky/ParkyAPI/Models/Trail.cs
+++ Parky/ParkyAPI/Models/Trail.cs
@@ -16,6 +16,8 @@ namespace ParkyAPI.Models
         [Required]
         public double Distance { get; set; }
         public enum DifficultyType { Easy, Moderate, Difficult, Expert }
+        [Required]
+        public double Elevation { get; set; }
         public DifficultyType Difficulty { get; set; }
         [Required]
         public int NationalParkId { get; set; }
